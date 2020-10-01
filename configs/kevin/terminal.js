c79e0891f46482b57361152a9e38b1b26a1c602c
Kevin Batdorf <kbat82@gmail.com>
2020-10-01 23:00:33 +0700

    Add terminal config

---
+++ configs/kevin/terminal.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      colors: {
+          night: '#000000',
+          hacker: '#76ff03',
+          shadow: '#315324',
+          connect: '#bdd0f4',
+      }
+    },
+    corePlugins: {
+      float: false,
+      objectFit: false,
+      objectPosition: false,
+    },
+  }
